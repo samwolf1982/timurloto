75749d672f245cb6d704aafb1ba9ccbd5d5a824f
Sam <biryukwolf@yahoo.com>
2018-12-13 06:34:00 +0200

    add bd session migration

---
+++ console/migrations/m181213_042448_create_sesionbd_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `sesionbd`.
+ */
+class m181213_042448_create_sesionbd_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('session', [
+            'id' => $this->primaryKey(),
+            'expire' => $this->integer(),
+            'data' => $this->binary(),
+
+        ]);
+    }
+
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('session');
+    }
+
+}
