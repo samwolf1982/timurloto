ed3e1885c28b7e5c1d2200fbe0aac1de651f62e5
Sam <biryukwolf@yahoo.com>
2018-12-16 21:32:31 +0200

    add hook

--- frontend/web/hooks/hook.php
+++ frontend/web/hooks/hook.php
@@ -4,12 +4,12 @@
 //header('Content-Type: text/html; charset=utf-8');
 if ( $_POST['payload'] ) {
     //shell_exec( ‘cd /var/www/drupalsites/git-repo/ && git reset –hard HEAD && git pull’ );
-
+    echo "<pre>".shell_exec( 'cd /home/lookmybe/lookmybets.com/lbrepo/ &&  git reset --hard HEAD  && git pull' )."</pre>";
+    var_dump(shell_exec('date'));
 }
 
 
-echo "<pre>".shell_exec( 'cd /home/lookmybe/lookmybets.com/lbrepo/ &&  git reset --hard HEAD  && git pull' )."</pre>";
-var_dump(shell_exec('date'));
+
 
 
 
