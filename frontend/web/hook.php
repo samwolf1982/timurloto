3c5467288e2a277b273a822e12bb157ac7ce62c6
Sam <biryukwolf@yahoo.com>
2018-12-16 20:27:45 +0200

    add hook

--- frontend/web/hook.php
+++ frontend/web/hook.php
@@ -4,7 +4,8 @@
 
 if ( $_POST['payload'] ) {
     //shell_exec( ‘cd /var/www/drupalsites/git-repo/ && git reset –hard HEAD && git pull’ );
-    shell_exec( "cd /var/www/drupalsites/git-repo/ && git reset –hard HEAD && git pull" );
+
 }
+ echo "<pre>".shell_exec( "cd /var/www/drupalsites/git-repo/ && git reset –hard HEAD && git pull" )."</pre>";
 
 ?>hi
