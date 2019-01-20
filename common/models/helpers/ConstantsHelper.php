240bd0fb3d38b1e96f4a653dd65c2dea3907d5ec
Sam <biryukwolf@yahoo.com>
2019-01-20 06:46:48 +0200

    переключение фин

--- common/models/helpers/ConstantsHelper.php
+++ common/models/helpers/ConstantsHelper.php
@@ -84,7 +84,7 @@ class ConstantsHelper
 
 ////////////////купленый парсер   or
     // const PARSE_BASE_URL='http://157.230.134.85:8081'; // not use
-    const PARSE_BASE_URL='http://157.230.134.85';
+//    const PARSE_BASE_URL='http://157.230.134.85';
 //    const PARSE_SPORT_TYPE_URL_PARTS='?lineSport/all/ru/j_zaxscdvfq1w2e3r4';  // 1
 //    //  const PARSE_TOURNEY_TYPE_URL_PARTS='getChampsBySportId/line/12341/ru/j_zaxscdvfq1w2e3r4'; // 2   not use
 //    const PARSE_TOURNEY_TYPE_URL_PARTS='?getChampsBySportId/line/%s/ru/j_zaxscdvfq1w2e3r4'; // 2
@@ -102,6 +102,7 @@ class ConstantsHelper
 //    const PARSE_BASE_URL='http://89.108.65.253:6543';
 //    const PARSE_BASE_URL='http://89.108.65.253';
 //    const PARSE_BASE_URL='http://89.108.65.253';
+    const PARSE_BASE_URL='http://104.248.229.40';
     const PARSE_SPORT_TYPE_URL_PARTS='?lineSport/all/ru/j_zaxscdvfq1w2e3r4';  // 1
     //  const PARSE_TOURNEY_TYPE_URL_PARTS='getChampsBySportId/line/12341/ru/j_zaxscdvfq1w2e3r4'; // 2
     const PARSE_TOURNEY_TYPE_URL_PARTS='?getChampsBySportId/line/%s/ru/j_zaxscdvfq1w2e3r4'; // 2
