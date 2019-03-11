22a770b86e2effebd6cb2f2cb974d869be904d10
Sam <biryukwolf@yahoo.com>
2019-03-11 21:22:26 +0200

    some fix lagre

--- common/models/Wager.php
+++ common/models/Wager.php
@@ -226,9 +226,12 @@ class Wager extends \yii\db\ActiveRecord
             if($returno){  $status = self::STATUS_ENTERED; $gate= false; }
         }
 
-        if($item->id==11 || $item->id==12 ){
-            yii::error(['id'=>$item->id,'stat'=>$item->status]);
+        foreach ($this->wagerelements as $item) {
+            if($item->id == 11 || $item->id==12 ){
+                yii::error(['id'=>$item->id,'stat'=>$item->status]);
+            }
         }
+
         // цикл проверок
         // 4 на присутсвие  STATUS_RETURN_BET STATUS_ENTERED // все исходы  ставка прошла но может быть один возврать
         if($gate){
