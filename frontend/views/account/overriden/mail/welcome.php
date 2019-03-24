2cfac678d95c5a24cb38f00025dc1b04bfc4d074
Sam <biryukwolf@yahoo.com>
2019-03-24 08:17:07 +0200

    modulb

--- frontend/views/account/overriden/mail/welcome.php
+++ frontend/views/account/overriden/mail/welcome.php
@@ -22,6 +22,7 @@ use yii\helpers\Html;
     <?= Yii::t('user', 'Добрый день') ?>,
 </p>
 
+
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
     <?= Yii::t('user', 'Ваш аккаунт в первой социальной сети для любителей ставок на спорт "{0}", был создан', Yii::$app->name) ?>.
     <?php if ($showPassword || $module->enableGeneratingPassword): ?>
