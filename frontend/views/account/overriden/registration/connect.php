1cdb19de94567e89b7111b9aba447bb60241cdd2
Sam <biryukwolf@yahoo.com>
2019-03-28 08:54:48 +0200

    news

--- frontend/views/account/overriden/registration/connect.php
+++ frontend/views/account/overriden/registration/connect.php
@@ -123,7 +123,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
                                                                     <a href="#" class="btn btn-small btn-primary" id="openMadaInner" data-toggle="modal-reg" data-target="#modal-auth"><i class="icon-user"></i> <span><?=  Yii::t(
                                                                                 'user',
-                                                                                'If you already registered, sign in and connect this account on settings page'
+                                                                                'If you already registered, sign in and connect this account on settings page');
                                                                         ?></span></a>
 
                                                                 </p>
