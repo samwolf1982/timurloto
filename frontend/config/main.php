22ff1c68c7b065d4f62bf032468303a6a0b88bf1
Sam <biryukwolf@yahoo.com>
2019-03-28 19:57:29 +0200

    news

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -125,6 +125,7 @@ return [
                 // первая регистрация через соцсеть
 
                 '/uregistration/connect/<code:(.*?)>'=>'user/registration/connect',
+                '/user/registration/connect/code:(.*?)>'=>'user/registration/connect',
 
 
 
