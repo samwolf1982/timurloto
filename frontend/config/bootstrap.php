370d834e542b22e478d2b477bce62d91a0a89e83
Sam <biryukwolf@yahoo.com>
2019-03-28 20:19:57 +0200

    news

--- frontend/config/bootstrap.php
+++ frontend/config/bootstrap.php
@@ -32,7 +32,7 @@ Event::on('dektrium\user\controllers\SecurityController', SecurityController::EV
                'username' => $e->account->username,
                'email'    => $e->account->email,
            ]);
-
+           Yii::$app->response->redirect(Url::to('/zzzz')); die();
 //        if(empty($user->email)) $user->email=$account->email;
 //           $event = $this->getConnectEvent($account, $user);
            Yii::createObject(['class' => ConnectEvent::className(), 'account' => $e->account, 'user' => $user]);
