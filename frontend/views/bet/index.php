a12678ebc60bcde252c57ac35b65d25ccee1b4cc
Sam <biryukwolf@yahoo.com>
2019-04-05 17:24:00 +0300

    add games

--- frontend/views/bet/index.php
+++ frontend/views/bet/index.php
@@ -396,11 +396,12 @@ BetAsset::register($this);
                                     <span class="text-head text-muted">25,000 ₽</span>
                                 </div>
 
+                                <select  onchange="if (this.value) window.location.href=this.value" >
+                                    <option value="<?=Url::toRoute(['/bet'])?>">Турнир текущий</option>
+                                    <option value="<?=Url::toRoute(['/bet'=>['dtop'=>'2019-03-31']])?>">2019-03-31 - 1 Турнир</option>
+                                    <option value="<?=Url::toRoute(['/bet'=>['dtop'=>'2019-03-24']])?>">2019-03-24 - 2 Турнир</option>
 
-                                <select >
-                                    <option value="t1">2019-03-24 - 1 Турнир</option>
-                                    <option value="t2">2019-03-31 - 2 Турнир</option>
-                                    <option value="t3">2019-04-07 - Турнир текущий</option>
+
                                 </select>
 
 
