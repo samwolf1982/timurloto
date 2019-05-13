dd8bf7f153601caabc56192f73aab2ce36e3cbcb
Sam <biryukwolf@yahoo.com>
2019-05-13 22:40:51 +0300

    tabe eok

--- common/models/services/AccessInfo.php
+++ common/models/services/AccessInfo.php
@@ -74,11 +74,12 @@ class AccessInfo
     {
 
         $lastWeek    = date('Y-m-d H:i:s');
-        $lastLastWeek= date('Y-m-d H:i:s',strtotime('last sunday + 1 day'));
+      //  $lastLastWeek= date('Y-m-d H:i:s',strtotime('last sunday + 1 day')); // бы
+        $lastLastWeek= date('Y-m-d H:i:s',strtotime('last sunday'));
         $userId=$this->user->id;
         $sql="SELECT  sum(profit) as sume FROM `balancestatistics` WHERE  created_own BETWEEN '{$lastLastWeek}' AND '{$lastWeek}' and user_id = {$userId} ;";
 
-//        var_dump($sql); die();
+      //  var_dump($sql); die();
         $numberWeek=Yii::$app->db->createCommand($sql)->queryScalar();
          if(empty($numberWeek)) $numberWeek=0;
         return  round( $numberWeek,2)  ;
