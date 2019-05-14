237a2776e2a57e961575239a216610bb617c1cda
Sam <biryukwolf@yahoo.com>
2019-05-14 04:05:20 +0300

    tabe eok

--- frontend/views/account/overriden/mail/recovery.php
+++ frontend/views/account/overriden/mail/recovery.php
@@ -17,7 +17,7 @@ use yii\helpers\Html;
  */
 ?>
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
-    <?= Yii::t('user', 'Добрый день') ?>,
+   hi lololo
 </p>
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
     <?= Yii::t('user', 'Mы отправили запрос на востановление пароля на сайте {0}', Yii::$app->name) ?>.
