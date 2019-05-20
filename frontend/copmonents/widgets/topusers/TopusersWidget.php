ab3fc6df74c477575fc65ba956e194a787d1c853
Sam <biryukwolf@yahoo.com>
2019-05-20 10:03:41 +0300

    модалки текстав

--- frontend/copmonents/widgets/topusers/TopusersWidget.php
+++ frontend/copmonents/widgets/topusers/TopusersWidget.php
@@ -16,18 +16,35 @@ class TopusersWidget extends Widget
 
     public $dataProvider;// провайдер данных как для грида
     public function run(){
-      //  $prewWeek_1='2019-03-31'; $prewWeek_2='2019-03-24';
-        $prewWeek_1=date("Y-m-d", strtotime("last week sunday -7 day"));
-        $prewWeek_2=date("Y-m-d", strtotime("last week sunday -14 day"));;
-        $prewWeek_3=date("Y-m-d", strtotime("last week sunday -21 day"));;
-        $prewWeek_4=date("Y-m-d", strtotime("last week sunday -28 day"));;
-        $prewWeek_5=date("Y-m-d", strtotime("last week sunday -35 day"));;
+        if(0){
+            //  $prewWeek_1='2019-03-31'; $prewWeek_2='2019-03-24';
+            $prewWeek_1=date("Y-m-d", strtotime("last week sunday -7 day"));
+            $prewWeek_2=date("Y-m-d", strtotime("last week sunday -14 day"));;
+            $prewWeek_3=date("Y-m-d", strtotime("last week sunday -21 day"));;
+            $prewWeek_4=date("Y-m-d", strtotime("last week sunday -28 day"));;
+            $prewWeek_5=date("Y-m-d", strtotime("last week sunday -35 day"));;
+
+            $prewWeek_1_text=date("Y-m-d", strtotime("last week sunday"));;
+            $prewWeek_2_text=date("Y-m-d", strtotime("last week sunday -7 day"));;
+            $prewWeek_3_text=date("Y-m-d", strtotime("last week sunday -14 day"));;
+            $prewWeek_4_text=date("Y-m-d", strtotime("last week sunday -21 day"));;
+            $prewWeek_5_text=date("Y-m-d", strtotime("last week sunday -28 day"));;
+        }
+
+
+        //  $prewWeek_1='2019-03-31'; $prewWeek_2='2019-03-24';
+        $prewWeek_1=date("Y-m-d", strtotime("last week sunday"));
+        $prewWeek_2=date("Y-m-d", strtotime("last week sunday -7 day"));;
+        $prewWeek_3=date("Y-m-d", strtotime("last week sunday -14 day"));;
+        $prewWeek_4=date("Y-m-d", strtotime("last week sunday -21 day"));;
+        $prewWeek_5=date("Y-m-d", strtotime("last week sunday -28 day"));;
 
         $prewWeek_1_text=date("Y-m-d", strtotime("last week sunday"));;
         $prewWeek_2_text=date("Y-m-d", strtotime("last week sunday -7 day"));;
         $prewWeek_3_text=date("Y-m-d", strtotime("last week sunday -14 day"));;
         $prewWeek_4_text=date("Y-m-d", strtotime("last week sunday -21 day"));;
         $prewWeek_5_text=date("Y-m-d", strtotime("last week sunday -28 day"));;
+
         return       $this->render('index', ['dataProvider'=>$this->dataProvider,'prewWeek_1'=>$prewWeek_1,'prewWeek_2'=>$prewWeek_2,
             'prewWeek_3'=>$prewWeek_3,'prewWeek_4'=>$prewWeek_4,'prewWeek_5'=>$prewWeek_5,
             'prewWeek_1_text'=>$prewWeek_1_text, 'prewWeek_2_text'=>$prewWeek_2_text, 'prewWeek_3_text'=>$prewWeek_3_text, 'prewWeek_4_text'=>$prewWeek_4_text, 'prewWeek_5_text'=>$prewWeek_5_text,
