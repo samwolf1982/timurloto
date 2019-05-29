2fefdedbff507beb61b1b6b005cbce8c6b1ace55
Sam <biryukwolf@yahoo.com>
2019-05-29 23:24:46 +0300

    mail

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -2,7 +2,7 @@
 return [
   //  'adminEmail' => 'info@lookmybets.com',
  //   'adminEmail' => 'My Name <info@lookmybets.com>"',
-    'adminEmail' => ' <My Name> info@lookmybets.com',
+     'adminEmail' => 'info@lookmybets.com',
     'showErrorOnPage404'=>false,
     'comment_user_list_id'=>[55,71,102,100,91,48,67,60,86,85,68,57], // cписок коментариев ид пользователей. текс коментарания вшыт в виджет CommentusersWidget количетво не более 12
 //    'excludeJseventName'=>[
